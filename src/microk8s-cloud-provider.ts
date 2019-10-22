d546b2835d55dac657118df6cae1d89dec2f4907
Ivan Towlson <ivan.towlson@microsoft.com>
2019-10-22 15:56:37 +1300

    Put a thing in the tree

--- src/microk8s-cloud-provider.ts
+++ src/microk8s-cloud-provider.ts
@@ -5,24 +5,52 @@ class Microk8sCloudProvider implements k8s.CloudExplorerV1.CloudProvider {
     readonly cloudName = "Microk8s";
     readonly treeDataProvider = new Microk8sTreeDataProvider();
     async getKubeconfigYaml(cluster: any): Promise<string | undefined> {
+        const treeNode = cluster as Microk8sCloudProviderTreeNode;
+        if (treeNode.nodeType === 'cluster') {
+            return await getMicrok8sKubeconfigYaml();
+        }
         return undefined;
     }
 }
 
+export interface Microk8sClusterNode {
+    readonly nodeType: 'cluster';
+}
+
+export interface Microk8sErrorNode {
+    readonly nodeType: 'error';
+    readonly diagnostic: string;
+}
+
 // TODO: the actual type
-export type Microk8sCloudProviderTreeNode = any;
+export type Microk8sCloudProviderTreeNode = Microk8sClusterNode | Microk8sErrorNode;
 
 class Microk8sTreeDataProvider implements vscode.TreeDataProvider<Microk8sCloudProviderTreeNode> {
     private readonly onDidChangeTreeDataEmitter: vscode.EventEmitter<Microk8sCloudProviderTreeNode | undefined> = new vscode.EventEmitter<Microk8sCloudProviderTreeNode | undefined>();
     readonly onDidChangeTreeData: vscode.Event<Microk8sCloudProviderTreeNode | undefined> = this.onDidChangeTreeDataEmitter.event;
 
     getTreeItem(element: Microk8sCloudProviderTreeNode): vscode.TreeItem | Thenable<vscode.TreeItem> {
-        return new vscode.TreeItem("TODO: render");
+        if (element.nodeType === 'error') {
+            const treeItem = new vscode.TreeItem("Error", vscode.TreeItemCollapsibleState.None);
+            treeItem.tooltip = element.diagnostic;
+            return treeItem;
+        } else {
+            const treeItem = new vscode.TreeItem("Microk8s Instance", vscode.TreeItemCollapsibleState.None);
+            treeItem.contextValue = `microk8s.cluster ${k8s.CloudExplorerV1.SHOW_KUBECONFIG_COMMANDS_CONTEXT}`;
+            return treeItem;
+        }
     }
 
     getChildren(element?: Microk8sCloudProviderTreeNode | undefined): vscode.ProviderResult<Microk8sCloudProviderTreeNode[]> {
-        return [];
+        if (element) {
+            return [];
+        }
+        return [{ nodeType: 'cluster' }];
     }
+}
+
+async function getMicrok8sKubeconfigYaml(): Promise<string | undefined> {
+    return undefined;
 }
 
 export const MICROK8S_CLOUD_PROVIDER = new Microk8sCloudProvider();
